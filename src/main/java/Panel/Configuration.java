a0fd9554f936f3ccf2f6a69b53f7cb9d4ff57b1b
automation <automation@qolsys.com>
2017-05-03 16:05:49 -0700

    paths

---
+++ src/main/java/Panel/Configuration.java
@@ -0,0 +1,30 @@
+package Panel;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Configuration {
+    private String config = "/home/automation/IdeaProjects/comqolsys2017/config.xls";
+    public String adbPath = getAdbPath();
+    public File appDir = new File ("scr");
+    public String udid_ = getudid_();
+
+    public Configuration() throws IOException, BiffException {}
+
+    public String getAdbPath() throws IOException, BiffException {
+        Workbook wb =Workbook.getWorkbook(new File(config));
+        Sheet sh = wb.getSheet(0);
+        String CellGetContent = sh.getCell(0,0).getContents();
+        return CellGetContent;
+    }
+    public String getudid_() throws IOException, BiffException {
+        Workbook wb =Workbook.getWorkbook(new File(config));
+        Sheet sh = wb.getSheet(0);
+        String CellGetContent = sh.getCell(1,0).getContents();
+        return CellGetContent;
+    }
+}
