0558127629782079ca29fd0cd1f3b3df48e94db5
qolsys <adyshleva@gmail.com>
2017-05-05 13:46:03 -0700

    Merge branch 'master' of https://github.com/adyshleva/comqolsys2017
    # Conflicts:
    #	Activity_Monitor_Page_Test.xml

--- src/main/java/Sensors/SensorsList.java
+++ src/main/java/Sensors/SensorsList.java
@@ -5,6 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SensorsList {
-    @FindBy(xpath = "//*[contains(text(), 'Door/Window 4')]")
+    @FindBy(xpath = "//android.widget.TextView[@text='Door/Window 4']")
     public WebElement Door4;
 }
