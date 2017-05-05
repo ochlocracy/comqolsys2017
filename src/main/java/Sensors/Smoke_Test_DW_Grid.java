1a2cabc6ddb64ce93c93e774824d837738dc6ef0
qolsys <adyshleva@gmail.com>
2017-05-05 10:38:04 -0700

    Merge branch 'master' of https://github.com/adyshleva/comqolsys2017
    # Conflicts:
    #	Activity_Monitor_Page_Test.xml

--- src/main/java/Sensors/Smoke_Test_DW_Grid.java
+++ src/main/java/Sensors/Smoke_Test_DW_Grid.java
@@ -38,8 +38,8 @@ public class Smoke_Test_DW_Grid  {
         logger.info("Current software version: " + s.Software_Version());
         MySensors.read_sensors_from_csv();
         logger.info("Adding sensors...");
-        MySensors.addAllSensors();
-        TimeUnit.SECONDS.sleep(20);
+        MySensors.addAllSensors1(UDID_);
+        TimeUnit.SECONDS.sleep(10);
 
         logger.info("Disarm mode tripping sensors group 10, 12, 13, 14, 16, 25 -> Expected result= system stays in Disarm mode");
         MySensors.sendPacket_allSensors_selectedGroup(MySensors.door_window_zones, 10, Open);
