bb853d8988a250f6a00677741e1bc09f3239f114
qolsys <adyshleva@gmail.com>
2017-07-24 15:37:42 -0700

    ADC SMoke

---
+++ src/main/java/ADC_Sanity_Test/RetryAnalizer.java
@@ -0,0 +1,20 @@
+package ADC_Sanity_Test;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalizer implements IRetryAnalyzer {
+
+    int counter =0;
+    int retryLimit = 2;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (counter <retryLimit)
+        {
+            counter++;
+            return true;
+        }
+        return false;
+    }
+}
