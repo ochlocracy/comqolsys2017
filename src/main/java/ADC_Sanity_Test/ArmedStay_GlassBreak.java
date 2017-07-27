c33c0c69f632e8125c2321fd7dd343af2860e3ee
adyshleva <adyshleva@gmail.com>
2017-07-27 11:01:15 -0700

    created practise package

--- src/main/java/ADC_Sanity_Test/ArmedStay_GlassBreak.java
+++ src/main/java/ADC_Sanity_Test/ArmedStay_GlassBreak.java
@@ -1,23 +1,23 @@
-package ADC_Sanity_Test;
-
-import Panel.Setup;
-import Sensors.Sensors;
-
-import java.util.logging.Logger;
-
-/**
- * Exit Delay
- * 1) trigger group 13 glass break sensor during exit delay
- * 2) trigger group 17 glass break sensor during exit delay
- *
- * Tamper
- * 1) tamper group 13 glass break
- * 2) tamper group 17 glass break
- */
-public class ArmedStay_GlassBreak extends Setup{
-    String page_name = "Arm Stay mode";
-    Logger logger = new Logger.getLogger();
-    private int Normal_Exit_Delay = 30000;
-
-}
-
+//package ADC_Sanity_Test;
+//
+//import Panel.Setup;
+//import Sensors.Sensors;
+//
+//import java.util.logging.Logger;
+//
+///**
+// * Exit Delay
+// * 1) trigger group 13 glass break sensor during exit delay
+// * 2) trigger group 17 glass break sensor during exit delay
+// *
+// * Tamper
+// * 1) tamper group 13 glass break
+// * 2) tamper group 17 glass break
+// */
+//public class ArmedStay_GlassBreak extends Setup{
+//    String page_name = "Arm Stay mode";
+//    Logger logger = new Logger.getLogger();
+//    private int Normal_Exit_Delay = 30000;
+//
+//}
+//
