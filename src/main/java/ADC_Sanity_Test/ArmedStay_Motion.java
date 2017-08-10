853032cb2eb28fc9915b41ebccd3fe6ee9498409
qolsys <adyshleva@gmail.com>
2017-08-10 15:48:09 -0700

    ADC Freeze

--- src/main/java/ADC_Sanity_Test/ArmedStay_Motion.java
+++ src/main/java/ADC_Sanity_Test/ArmedStay_Motion.java
@@ -276,9 +276,9 @@ public class ArmedStay_Motion extends Setup{
     @AfterTest
     public void tearDown() throws IOException, InterruptedException {
         driver.quit();
-//        for (int i= 5; i>0; i--) {
-//            delete_from_primary(i);
-//        }
+        for (int i= 5; i>0; i--) {
+            delete_from_primary(i);
+        }
     }
 
     @AfterMethod
