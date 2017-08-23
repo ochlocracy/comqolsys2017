27400e64d4b3d9be194073554c445c96d8c16e3b
Nathan Chortek <nathan.chortek@qolsys.com>
2017-08-23 10:11:11 -0700

    fixed issues with update settings, now working on user codes

--- src/main/java/Update/PreUpdate_UserManagement.java
+++ src/main/java/Update/PreUpdate_UserManagement.java
@@ -1,25 +1,26 @@
 package Update;
 
-import Panel.Advanced_Settings_Page;
-import Panel.Home_Page;
-import Panel.Setup;
-import Panel.User_Management_Page;
+import ADC.ADC;
+import Panel.*;
 import jxl.read.biff.BiffException;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PreUpdate_UserManagement extends Setup {
 
     String page_name = "PreUpdate User Management set up";
     Logger logger = Logger.getLogger(page_name);
+    ADC adc = new ADC();
+    PanelInfo_ServiceCalls servcall = new PanelInfo_ServiceCalls();
 
     public PreUpdate_UserManagement() throws IOException, BiffException {}
 
@@ -38,11 +39,29 @@ public class PreUpdate_UserManagement extends Setup {
             driver.hideKeyboard();
         } catch (Exception e) {}
     }
+    public void checkCodes(List<WebElement> li, List<String> exp, int size){
+        logger.info("Checking user codes on ADC");
+        int i;
+
+        for(i = 2; i < size; i++){
+            String s1 = li.get(i).getText().split(" ")[0];
+            String s2 = exp.get(i-2);
+            if(s1.equals(s2))
+                logger.info("Pass: User Code " + s2 + " is displayed.");
+            else
+                logger.info("Fail: " + s1 + " is displayed. Expected " + s2);
+        }
+    }
 
     @BeforeTest
     public void capabilities_setup() throws Exception {
         setup_driver(get_UDID(),"http://127.0.1.1", "4723");
         setup_logger(page_name);
+    }
+
+    @BeforeMethod
+    public void webDriver() {
+        adc.webDriverSetUp();
     }
 
     @Test
@@ -56,7 +75,7 @@ public class PreUpdate_UserManagement extends Setup {
         addUser("NewUser", "5643");
         user_m.Add_User_add_page.click();
         home.Home_button.click();
-        Thread.sleep(1000);
+        Thread.sleep(4000);
     }
     @Test (priority = 1)
     public void verifyNewUserCodeWorks() throws Exception {
@@ -133,6 +152,27 @@ public class PreUpdate_UserManagement extends Setup {
         verify_disarm();
     }
     @Test (priority = 6)
+    public void addDuress() throws IOException, InterruptedException {
+        servcall.set_DURESS_AUTHENTICATION_enable();
+    }
+    @Test (priority = 7)
+    public void verifyADC() throws InterruptedException {
+        TimeUnit.MINUTES.sleep(3);
+        adc.navigate_to_user_site("Gen2-8334", "qolsys1234");
+        adc.getDriver1().findElement(By.id("Users")).click();
+        adc.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/form/div[4]/div[2]/div/" +
+                "div[3]/div[3]/div[1]/div/div/div[2]/div/div[2]/a[1]/span[1]/span"))).click();
+        adc.getDriver1().findElement(By.id("ctl00_HeaderLinks1_imgReload")).click();
+        Thread.sleep(3000);
+        List<WebElement> li = adc.getDriver1().findElements(By.id("pinCode"));
+        List<String> expected = Arrays.asList("5643", "3331", "8800", "9998");;
+
+        int size;
+        size = li.size();
+        checkCodes(li, expected, size);
+    }
+
+    @Test (priority = 8)
     public void deleteNewUsers() throws Exception {
         logger.info("Adding a new Guest NewGuest with the code 8800");
         User_Management_Page user_m =  PageFactory.initElements(driver, User_Management_Page.class);
@@ -145,7 +185,32 @@ public class PreUpdate_UserManagement extends Setup {
         }
         Thread.sleep(1000);
         home.Home_button.click();
+        servcall.set_DURESS_AUTHENTICATION_disable();
     }
+    @Test (priority = 8)
+    public void adcDelete() throws InterruptedException {
+        adc.navigate_to_user_site("Gen2-8334", "qolsys1234");
+        adc.getDriver1().findElement(By.id("Users")).click();
+        Thread.sleep(3000);
+        adc.getDriver1().findElement(By.id("ctl00_HeaderLinks1_imgReload")).click();
+        Thread.sleep(3000);
+        int i;
+        for(i = 0; i < 4; i++) {
+            logger.info("deleting user code");
+            adc.getDriver1().findElement(By.xpath("/html/body/div/form/div[4]/div[2]/div/div[3]/div[3]/div[1]/div/div/div[5]/div[2]/div[2]/div[1]/a")).click();
+            logger.info("confirming deletion");
+            adc.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/form/div[4]/div[2]" +
+                    "/div/div[3]/div[3]/div[1]/div/div/div[7]/div[1]/div/div/div[3]/button[1]"))).click();
+        }
+
+
+    }
+
+    @AfterMethod
+    public void webDriverQuit() {
+        adc.getDriver1().quit();
+    }
+
     @AfterTest
     public void tearDown() throws IOException, InterruptedException {
         driver.quit();
