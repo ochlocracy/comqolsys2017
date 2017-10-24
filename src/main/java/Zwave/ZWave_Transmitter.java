d2e99ec4e6b2c265f2907a6f60d4ea35d48e7dda
adyshleva <adyshleva@gmail.com>
2017-10-24 16:57:57 -0700

    Setting up Transmitter.

---
+++ src/main/java/Zwave/ZWave_Transmitter.java
@@ -0,0 +1,32 @@
+package Zwave;
+
+import Panel.Setup;
+
+/**
+ * Created by qolsys on 10/24/17.
+ *
+ *
+ * Methods to add
+ * -Add transmitter
+ * -delete transmitter
+ * -pair lights
+ * -pair thermostat
+ * -pair door lock
+ * -pair Garage Door
+ * -pair Other devices
+ * -pair unsupported devices
+ * -turn on light
+ * -turn off light
+ * -turn on all lights
+ * -turn off all lights
+// *
+// */
+//public class ZWave_Transmitter extends Setup {
+//
+//
+//
+//
+//
+//
+//
+//}
