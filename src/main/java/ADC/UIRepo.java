bf627f232c6df2e55f3d17074cb1a25ac0827321
adyshleva <adyshleva@gmail.com>
2017-10-30 16:57:09 -0700

    Sanity

---
+++ src/main/java/ADC/UIRepo.java
@@ -0,0 +1,17 @@
+package ADC;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+public class UIRepo {
+
+    @FindBy(xpath = "//div[contains(@class, 'icon ') and contains(@title, 'Disarmed ')]")
+    public WebElement Disarm_state;
+    @FindBy(xpath = "//button[contains(@id, 'ember') and contains(@class, 'armed-stay btn ember-view')]")
+    public WebElement Arm_Stay;
+    @FindBy(xpath = "//div[contains(@class, 'icon ') and contains(@title, 'Armed Stay ')]")
+    public WebElement Arm_Stay_state;
+    @FindBy(xpath = "//button[contains(@id, 'ember') and contains(@class, 'disarmed btn ember-view')]")
+    public WebElement Disarm;
+}
